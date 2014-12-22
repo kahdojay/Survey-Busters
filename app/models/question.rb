086e911b220dcc4032fdba8bc12173b3d524d917
haileyr <hangyeolryu@gmail.com>
2014-12-22 12:21:19 -0500

    write find_stat for a question in the question model and test on console is successful without any data

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,19 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :survey
   validates_presence_of :description
+
+
+  def find_stat
+    answer_stat_hash = {}
+    answers_to_the_question = Answer.where(question_id: self.id)
+    answers_to_the_question.each do |answer|
+      answer_stat_hash[answer.description] = Selection.where(answer_id: answer.id).count
+    end
+    total_response_count = answer_stat_hash.values.sum
+    answer_stat_hash.each do |des, count|
+      answer_stat_hash[des] = (count/total_response_count) * 100
+    end
+    answer_stat_hash
+  end
+
 end
