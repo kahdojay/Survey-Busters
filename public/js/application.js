db0d8311900f8ea28e20e53179095486c7055704
haileyr <hangyeolryu@gmail.com>
2014-12-22 12:48:33 -0500

    display questions for a survey with part layout(_show_each.erb)

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('#survey_submit').on('submit', function(){
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  });
 });
