fe6ab250b489d0e29ae9490c41a3612a8f699008
haileyr <hangyeolryu@gmail.com>
2014-12-22 17:33:09 -0500

    pass response from survey radio button to survey.rb

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -2,3 +2,20 @@ get '/survey/:id' do |id|
   @survey = Survey.find(id)
   erb :'survey/take_survey', locals: {survey: @survey}
 end
+
+
+get '/survey/:id/stat' do |id|
+  params.each do |key, answer_id|
+    next if !(key.match("response"))
+    s = Selection.create(answer_id: answer_id.to_i)
+    s.user_id = current_user.id if current_user
+    p s
+  end
+
+  @survey = Survey.find(id)
+
+
+  #
+  # stats = @survey.questions.map {|q| q.find_stat}
+  # erb :'survey/take_survey', locals: {survey: @survey}
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-  $('#survey_submit').on('submit', function(){
+  $('#survey_submit').on('submit', function(event){
+    event.preventDefault();
+    if (check_all_responded()){
+      var $target = $(event.target);
+      $.ajax({
+        url: $target.attr('action'),
+        type: 'GET',
+        data: $target.serialize()
+      }).done(function(response){
+        console.log(response);
+      });
+    }
+    else{
+      alert("Some of the questions are not answered.");
+    }
 
   });
 });
+
+
+function check_all_responded(){
+  return $('input[type="radio"]:checked').length == 2;//$('.question').length;
+};
+
+// function get_answer_id(element){
+//   element.name.split('_')[1]
+// };
+
+function get_all_responses(){
+  answered_ids = [];
+  $('input[type="radio"]:checked').toArray().forEach(function(answer) {
+    answered_ids.push(answer.name.split('_')[1]);
+    });
+  console.log(answered_ids);
+  return answered_ids;
+};
+
