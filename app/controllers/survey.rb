5d4115591753620c70613aa44662cc03486e8576
Don J <kahdojay@gmail.com>
2014-12-24 18:20:07 -0500

    Change 'entry' (from old copy pasted code) to 'survey', rearranging routes along the way.

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,3 +1,8 @@
+get '/surveys' do
+  @surveys = Survey.all
+  erb :'survey/all'
+end
+
 get '/survey/:id' do |id|
   @survey = Survey.find(id)
   erb :'survey/take_survey', locals: {survey: @survey, stat_array: nil}
@@ -15,4 +20,37 @@ get '/survey/:id/stat' do |id|
   @survey = Survey.find(id)
   stat_array = @survey.questions.map {|q| q.find_stat}
   erb :'survey/take_survey', locals: {survey: @survey, stat_array: stat_array, questions: @survey.questions}, layout: false
+end
+
+post '/surveys/new' do
+  new_survey = Survey.create(params[:survey])
+  if new_survey.valid?
+    new_survey.save
+    redirect('/surveys')
+  else
+    @error = "Please write something."
+    erb :main
+  end
+end
+
+get '/survey/:id/edit' do |id|
+  if session[:user_id] == current_user.id
+    @survey = Survey.find(id)
+    erb :'survey/edit'
+  else
+    redirect('/')
+  end
+end
+
+put '/survey/:id/edit' do |id|
+  @survey = Survey.find(id)
+  redirect("/survey/#{@survey.id}")
+end
+
+delete '/survey/:id' do |id|
+  if session[:user_id] == current_user.id
+    survey = Survey.find(id)
+    survey.delete!
+  end
+  redirect('/surveys')
 end
