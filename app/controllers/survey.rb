4cc29aeee26e1830257d7a2777a173d85dc135da
Mike Horn & Ricky Thomas <pair+mikeh0rn.ricky-thomas@devbootcamp.com>
2014-12-30 14:54:16 -0500

    Creates edit page for after a new survey is given a title

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,3 +1,8 @@
+# get '/:id/survey/:survey_id' do
+#   @survey = Survey.find(params[:survey_id])
+#   erb :'survey/show'
+# end
+
 #create a new survey
 get '/:id/survey/new' do |id|
   if session[:user_id] != id.to_i
@@ -7,10 +12,19 @@ get '/:id/survey/new' do |id|
   end
 end
 
+
+#Instantiates new survey after clicking add title, and redirects to edit
 post '/:id/survey/new' do |id|
   @new_survey = Survey.create(title: params['new_survey']['title'], user_id: id)
-  erb :'survey/create_survey'
+  redirect("#{id}/survey/#{@new_survey.id}/edit")
 end
+
+
+get '/:id/survey/:survey_id/edit' do |id, survey_id|
+  @survey = Survey.find(survey_id)
+  erb :'survey/edit'
+end
+
 
 
 
