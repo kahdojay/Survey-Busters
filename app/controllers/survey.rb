83b8fbb163c9e13002836db1863ddc0f2defd47c
Mike Horn & Ricky Thomas <pair+mikeh0rn.ricky-thomas@devbootcamp.com>
2014-12-30 14:12:34 -0500

    Creates Survey upon adding title. Includes byebug in gemfile

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,3 +1,4 @@
+#create a new survey
 get '/:id/survey/new' do |id|
   if session[:user_id] != id.to_i
     redirect('/')
@@ -6,7 +7,15 @@ get '/:id/survey/new' do |id|
   end
 end
 
-get '/survey/:id' do |id|
-  @survey = Survey.find(id)
-  erb :'survey/take_survey', locals: {survey: @survey}
+post '/:id/survey/new' do |id|
+  @new_survey = Survey.create(title: params['new_survey']['title'], user_id: id)
+  erb :'survey/create_survey'
 end
+
+
+
+#routing to take survey page
+# get '/survey/:id' do |id|
+#   @survey = Survey.find(id)
+#   erb :'survey/take_survey', locals: {survey: @survey}
+# end
